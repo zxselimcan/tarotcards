dfd8c3bf4fef3b7d296b7a05a6695862c6f014cf
zxselimcan <zxselimcan@icloud.com>
2024-09-21 02:39:06 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import CardsJSON from "./cards.json";
+import { motion } from 'framer-motion';
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+const TarotCard = ({ card }: { card: string }) => {
+  const [flipped, setFlipped] = useState(true);
+
+  const cardFront = `/cards/${card}.jpg`;
+  const cardBack = `/cards/card-back.jpg`;
+
+  const wAfterScaleDown = `w-[calc(350px/4.2)]`;
+  const hAfterScaleDown = `h-[calc(600px/4.2)]`;
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className={`card-container shadow-lg relative rounded-lg overflow-hidden ${wAfterScaleDown} ${hAfterScaleDown} `}>
+      <motion.div
+        whileTap={{ scale: 0.9 }}
+        transition={{ duration: 0.2 }}
+        onClick={() => setFlipped(false)}
+        className={`card ${flipped ? 'flipped' : ''} ${wAfterScaleDown} ${hAfterScaleDown}`}
+      >
+        <div
+          className={`card-face card-front ${wAfterScaleDown} ${hAfterScaleDown}`}
+          style={{ backgroundImage: `url(${cardFront}) ` }}
+        ></div>
+        <div
+          className={`card-face card-back ${wAfterScaleDown} ${hAfterScaleDown}`}
+          style={{ backgroundImage: `url(${cardBack})` }}
+        ></div>
+      </motion.div>
     </div>
   );
+};
+
+
+const Page = () => {
+
+  const [cards, setCards] = useState<string[]>([])
+
+  useEffect(() => {
+
+    const shuffle = (array: string[]): string[] => {
+      let currentIndex = array.length;
+      while (currentIndex != 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+      return array;
+    }
+
+    let cardsShuffled = [...CardsJSON];
+
+    for (let i = 0; i < 22; i++) {
+      cardsShuffled = shuffle(cardsShuffled);
+    }
+    setCards(cardsShuffled);
+
+    return () => {
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="my-4 flex justify-center gap-4">
+        <Button
+          variant={"destructive"}
+          onClick={() => {
+            window.location.reload();
+          }}
+        >
+          Tekrar Karıştır
+        </Button>
+      </div>
+      <div className="flex flex-wrap justify-center items-start gap-2 mt-2">
+        {
+          cards.length > 0 && cards.map((card) => <TarotCard key={card} card={card} />)
+        }
+      </div>
+
+    </div>
+  )
 }
+
+export default Page;
